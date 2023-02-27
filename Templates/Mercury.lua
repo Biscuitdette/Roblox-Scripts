798bf62ac2ca46b41396b7054b52eac27e6cd8c2
Biscuitdette <120401315+Biscuitdette@users.noreply.github.com>
2023-02-27 22:28:26 +0100

    Update Mercury.lua

--- Templates/Mercury.lua
+++ Templates/Mercury.lua
@@ -896,7 +896,7 @@ function Library:create(options)
 
 	rawset(mt, "creditsContainer", creditsTab.container)
 
-	creditsTab:credit{Name = "Biscuitdette", Description = "Developpeur", Discord = "Biscuitdette#0001"}
+	creditsTab:credit{Name = "Biscuitdette", Description = "Développeur", Discord = "Biscuitdette#0001"}
 	creditsTab:credit{Name = "C3pitain", Description = "Testeur", Discord = "Dwalen#0237"}
 	creditsTab:credit{Name = "Lopotichat", Description = "Testeur", Discord = "lopotichat#7936"}
         creditsTab:credit{Name = "GitHub", Description = "L'endroit de mes publications", GitHub = "https://github.com/Biscuitdette"}
