d3cc2a64a4c01c5c1ffaef67af27dbebd2dff494
Biscuitdette <120401315+Biscuitdette@users.noreply.github.com>
2023-02-28 11:26:38 +0100

    Update Mercury.lua

--- Templates/Mercury.lua
+++ Templates/Mercury.lua
@@ -862,7 +862,7 @@ function Library:create(options)
 	settingsTab:keybind{
 		Name = "Affichage de l'interface",
 		Description = "Veuillez entrer une KeyBind pour masquer/apparaitre l'interface.",
-		Keybind = Enum.KeyCode.ALT,
+		Keybind = Enum.KeyCode.Delete,
 		Callback = function()
 			self.Toggled = not self.Toggled
 			Library:show(self.Toggled)
